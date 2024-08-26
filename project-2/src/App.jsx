7aec74f221e019c2388c5ccf4cee5d2a4ee91602
mihir1816 <janmihir181@gmail.com>
2024-08-26 15:18:16 +0530

    counter and error msg

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -4,30 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(10)
+  let [msg , setmsg]= useState(" ")
+
+  const addone  = () => {
+    if( count < 20 ){
+      setCount(count + 1) ;
+      setmsg("")
+    } else {
+      setmsg(" ERROR : you reach the maximum limit ")
+    }
+  }
+  const reducevalue = () => {
+    if( count > 0 ){
+      setCount(count -1 )
+      setmsg("")
+    } else {
+      setmsg("ERROR : you reach the minimum limit ")
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>MY Counter </h1>
+    <h3>value : {count}</h3>
+
+    <button onClick={addone}>add</button>
+    <button onClick={reducevalue}> reduce</button>
+    <h4>{ msg}</h4>
     </>
   )
 }
